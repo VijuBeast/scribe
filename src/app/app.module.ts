6312c0e4d85740539229fa0c5fb44e68f900fab2
Viju <vsbrth.apache@gmail.com>
2020-06-28 10:54:53 +0530

    Anglar Project Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,63 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CapitalizePipe } from './capitalize.pipe';
+import { MenuComponent } from './menu/menu.component';
+import { MyblogsComponent } from './myblogs/myblogs.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CreateComponent } from './create/create.component';
+import { NgxTextEditorModule } from 'ngx-text-editor';
+import { HttpClientModule } from '@angular/common/http';
+import { PostComponent } from './post/post.component';
+import { ViewComponent } from './view/view.component';
+import { CommentsComponent } from './comments/comments.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+
+let firebaseConfig = {
+  apiKey: 'AIzaSyArx60MBNXr4U9v5wUxV1DHKjQgJXNrWbw',
+  authDomain: 'scribe-9a9f6.firebaseapp.com',
+  databaseURL: 'https://scribe-9a9f6.firebaseio.com',
+  projectId: 'scribe-9a9f6',
+  storageBucket: 'scribe-9a9f6.appspot.com',
+  messagingSenderId: '851191679383',
+  appId: '1:851191679383:web:8e076d0fb3ab341ace9cd2',
+  measurementId: 'G-WVE8C13Y42',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    CapitalizePipe,
+    MenuComponent,
+    MyblogsComponent,
+    ProfileComponent,
+    CreateComponent,
+    PostComponent,
+    ViewComponent,
+    CommentsComponent,
+    EditProfileComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxTextEditorModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
